ef5751b390d1e5bebc250d5948068993b8360479
Stephanie La Raja <stephanielaraja@gmail.com>
2021-02-09 20:50:14 -0500

    commit tree 5a5cbb7486928f315f6c2a4b6c3b8d3642e7a7b2

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,28 @@
+class Patient 
 
+    attr_accessor :name 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def appointments 
+        Appointment.all.select {|appointment| appointment.patient == self}
+    end 
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end 
+
+    def doctors 
+        appointments.map {|doctor_name| doctor_name.doctor}
+    end 
+
+end 
